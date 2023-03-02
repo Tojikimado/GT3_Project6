26cdb96e26ed581fe6b971333146d9306888d3bc
MAGNINAntoine <115634385+MAGNINAntoine@users.noreply.github.com>
2023-03-02 21:46:33 +0100

    maj door

--- Assets/Script/passwordmanager.cs
+++ Assets/Script/passwordmanager.cs
@@ -7,12 +7,14 @@ public class passwordmanager : ScriptableObject
     private string solution;
     [SerializeField]
     private string RightSolution = "1234";
+    [SerializeField] private Door door;
      public void test(string u)
     {
         solution += u;
            if (solution.Contains(RightSolution))
             {
                 Debug.Log("WIN");
+                door.OpenCloseDoor();
             }
               Debug.Log(solution);
     }

bb50b2e6f804dfd7bd9f4fbe21d4028ef91ce93b
Sh6pa <71717569+Sh6pa@users.noreply.github.com>
2023-03-03 04:36:54 +0100

    Attempt2

--- Assets/Scripts/PlayerPreciseDetection.cs
+++ Assets/Scripts/PlayerPreciseDetection.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 using UnityEngine.XR;
@@ -6,42 +7,59 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerPreciseDetection : MonoBehaviour
 {
-    [SerializeField]
-    private InputDevice LeftControllerDevice;
-    [SerializeField]
-    private InputDevice RightControllerDevice;
+    private InputDevice LeftController;
+    private InputDevice RightController;
 
-    private Vector3 LeftControllerVelocity;
-    private Vector3 RightControllerVelocity;
+    private float LeftControllerVelocity;
+    private float RightControllerVelocity;
 
     [SerializeField]
     private PlayerMovementDetection _PlayerMovementDetection;
 
     void Start()
     {
-        LeftControllerDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
-        RightControllerDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        LeftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        RightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+    }
+
+    private void InitializeInputDevices()
+    {
+        if (!LeftController.isValid)
+            InitializeInput(InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Left, ref LeftController);
+        if (!RightController.isValid)
+            InitializeInput(InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right, ref RightController); 
+    }
+
+    private void InitializeInput(InputDeviceCharacteristics inputChar, ref InputDevice inputDevice)
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(inputChar, devices);
+        if (devices.Count > 0)
+        {
+            inputDevice = devices[0];
+        }
     }
     void Update()
     {
-        UpdateInput();
-        Debug.Log($"Left vel : {LeftControllerVelocity.magnitude}, Right Vel : {RightControllerVelocity.magnitude}, SpeedTest : {_PlayerMovementDetection.SpeedDetection}");
-        if (LeftControllerVelocity.magnitude > _PlayerMovementDetection.SpeedDetection)
+        if (!RightController.isValid || !LeftController.isValid)
+            InitializeInputDevices();
+        Vector3 tempLeftVel;
+        Vector3 tempRightVel;
+        LeftController.TryGetFeatureValue(CommonUsages.deviceVelocity, out tempLeftVel);
+        RightController.TryGetFeatureValue(CommonUsages.deviceVelocity, out tempRightVel);
+        LeftControllerVelocity = tempLeftVel.magnitude;
+        RightControllerVelocity = tempRightVel.magnitude;
+        Debug.Log($"Left:{LeftControllerVelocity}, Right:{RightControllerVelocity}");
+        if (LeftControllerVelocity > _PlayerMovementDetection.SpeedDetection)
         {
             _PlayerMovementDetection.SetIsMoving(true);
-
-        } else if (RightControllerVelocity.magnitude > _PlayerMovementDetection.SpeedDetection)
+        } else if (RightControllerVelocity > _PlayerMovementDetection.SpeedDetection)
         {
             _PlayerMovementDetection.SetIsMoving(true);
         } else
         {
             _PlayerMovementDetection.SetIsMoving(false);
         }
-    }
-    void UpdateInput()
-    {
-        LeftControllerDevice.TryGetFeatureValue(CommonUsages.deviceVelocity, out LeftControllerVelocity);
-        RightControllerDevice.TryGetFeatureValue(CommonUsages.deviceVelocity, out RightControllerVelocity);
     }
 
 }
